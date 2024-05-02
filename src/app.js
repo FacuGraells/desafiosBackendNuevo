26e7119bc1e44d4dfb2c71de9c79ca93a0e1defd
facundo graells <facugraells4@gmail.com>
2024-05-02 18:47:26 -0300

    solucion desafio 5

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 import { Express } from "express";
+import {server} from "socket.io";
+import {engine} from "express-handlebars";
+
 import products from "./routers/products.js"
 import carts from "./routers/cart.js"
+
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+
+app.engine('handlebars', engine());
+app.set('views', __dirname + './views');
+app.set('view engine', 'handlebars');
 
 app.get("/", (req, res) => {
-    return  res.status(200).send("bienvenido a la API")
+    return  res.rneder("home")
 });
 
 
